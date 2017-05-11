968598bc79eb925e97c9f77ba0cf9030675c3de5
Gerald Morna <Gerald@Geralds-MacBook-Air.local>
2017-05-11 18:43:38 +1000

    Initial Commit

---
+++ FitLift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitLift
+//
+//  Created by Gerald Morna on 11/5/17.
+//  Copyright © 2017 Gerald Morna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
